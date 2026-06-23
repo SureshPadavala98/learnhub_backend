91116bcb85132a80ea46f9b36630392b53085987
SureshPadavala98 <sureshpadavala0998@gmail.com>
2026-06-23 12:36:59 +0530

    Implement course category management; add serializers, views, and URLs for course categories, and define custom permissions for user roles

--- super_admin/versioned/v1/urls/admin_urls.py
+++ super_admin/versioned/v1/urls/admin_urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from super_admin.versioned.v1.views import (
     user_views
 )
+from super_admin.versioned.v1.views import (
+    courses_view
+)
 
 urlpatterns = [
     path('list/',user_views.MentorListAPIView.as_view(),name='mentors-list'),
+
+    # Course Category
+    path('course-category/', courses_view.CourseCategoryAPIVIEW.as_view(), name='course-categories'),
+    path('course-category-detail/<uuid:category_id>/', courses_view.CourseCategoryDetailAPIVIEW.as_view(), name='course-categories'),
 
 ]
